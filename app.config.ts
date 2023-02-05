42a6e1e539acaaffa1c23b4642244136593787ff
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-05 07:09:17 +0000

    Initial Commit

---
+++ app.config.ts
@@ -0,0 +1,19 @@
+export default defineAppConfig({
+  docus: {
+    title: 'EasyScript.ts',
+    description: 'Write code in javascript or typescript, much easier enjoy the power of EasyScript with a blazing fast developer experience.',
+    socials: {
+      twitter: 'EasyScriptJS',
+      github: 'EasyScriptJS',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {}
+  }
+})
